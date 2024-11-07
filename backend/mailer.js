00f44fc422f8e910483ceb132b9248940a1f5950
Esm322 <86346824+Esm322@users.noreply.github.com>
2024-11-07 21:31:56 +0300

    update nginx and docker

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const nodemailer = require('nodemailer');
 const app = express();
 
