612dc04b99553cec1d2f22aae157c9261028e179
Esm322 <86346824+Esm322@users.noreply.github.com>
2024-11-18 17:47:06 +0300

    fixed burger menu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import style from './assets/styles/main.scss';
 import hr from './assets/img/hr.svg';
-import favicon from './assets/img/favicon.svg';
 import checkmarkSVG from './assets/img/checkmark.svg';
 import robot from './robots.txt';
 import sitemap from './sitemap.xml';
@@ -8,36 +7,36 @@ import favi from './assets/img/favicon.ico'
 
 const URL = 'https://api.hr-consultingkrd.ru/feedback';
 
-const burgerBtn = document.querySelector('.menu__btn');
-const burgerWrapper = document.querySelector('.hamburger-menu');
-const burgerLinks = document.querySelectorAll('.menu__item');
+const burgerBtn = document.querySelector('.hamburger-menu');
+const customBtn = document.querySelectorAll('.custom-hamburger');
+const burgerWrapper = document.querySelector('.nav-links');
+const burgerLinks = document.querySelectorAll('.nav-links__link');
 const stickyTitles = document.querySelectorAll('.sticky');
 const form = document.querySelector('.form');
 const checkmark = document.querySelector('.contacts__checkmark-text');
 const inputs = document.querySelectorAll('.contacts__input');
 const textarea = document.querySelector('.contacts__textarea');
 
-window.addEventListener('DOMContentLoaded', () => {
-  if (window.innerWidth <= 768) {
-    stickyTitles.forEach((title) => title.classList.remove('sticky'));
+if (window.innerWidth <= 768) {
+  stickyTitles.forEach((title) => title.classList.remove('sticky'));
+}
 
-    burgerBtn.addEventListener('click', () => {
-      burgerWrapper.classList.toggle('burger--active')
+burgerBtn.addEventListener('click', () => {
+  customBtn.forEach((span) => span.classList.toggle('hamburger--active'));
+  burgerWrapper.classList.toggle('show-menu');
 
-      if (burgerWrapper.classList.contains('burger--active')) {
-        document.body.classList.add('overflow--off');
-      } else {
-        document.body.classList.remove('overflow--off');
-      }
-    })
-
-    burgerLinks.forEach((link) => {
-      link.addEventListener('click', () => {
-        document.body.classList.remove('overflow--off');
-        document.body.classList.add('overflow--off');
-      })
-    })
+  if (burgerWrapper.classList.contains('show-menu')) {
+    document.body.classList.add('overflow--off');
+  } else {
+    document.body.classList.remove('overflow--off');
   }
+})
+
+burgerLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('overflow--off');
+    document.body.classList.add('overflow--off');
+  })
 })
 
 inputs.forEach((input) => {
